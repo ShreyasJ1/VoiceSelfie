be7c7ac70fb95ce537ecf426139535bc777c9056
ShreyasJ1 <109956165+ShreyasJ1@users.noreply.github.com>
2023-02-26 14:58:53 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 var speechrecognition = window.webkitSpeechRecognition;
 var recognition = new speechrecognition();
 
-function start()
-{
-document.getElementById("textbox").innerHTML = "";
-recognition.start();
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
 }
-recognition.onresult = function(event)
-{
-console.log(event);
-var content = event.results[0][0].transcript;
-console.log(content);
-document.getElementById("textbox").innerHTML = content;
-
+recognition.onresult = function (event) {
+    console.log(event);
+    var content = event.results[0][0].transcript;
+    console.log(content);
+    document.getElementById("textbox").innerHTML = content;
+    speak();
 }
+function speak() {
+    var synth = window.speechSynthesis;
+    speech_data = document.getElementById("textbox").value;
+    var utterthis = new SpeechSynthesisUtterance(speech_data);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+}
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
