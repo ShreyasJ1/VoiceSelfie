bd38cb7a1bcd411db70676297d65098dace6124f
ShreyasJ1 <109956165+ShreyasJ1@users.noreply.github.com>
2023-03-04 15:05:10 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -9,15 +9,22 @@ recognition.onresult = function (event) {
     console.log(event);
     var content = event.results[0][0].transcript;
     console.log(content);
-    document.getElementById("textbox").innerHTML = content;
+    if (content == "take my selfie"){
+    console.log("taking selfie");
     speak();
+    }
+    document.getElementById("textbox").innerHTML = content;
 }
 function speak() {
     var synth = window.speechSynthesis;
-    speech_data = document.getElementById("textbox").value;
+    speech_data = "taking your selfie in 5 seconds";
     var utterthis = new SpeechSynthesisUtterance(speech_data);
     synth.speak(utterthis);
     Webcam.attach(camera);
+    setTimeout(function(){
+    take_snapshot();
+    save();
+    },5000);
 }
 Webcam.set({
     width: 360,
@@ -26,3 +33,18 @@ Webcam.set({
     png_quality: 90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+ data_uri + '">';
+});
+}
+
+function save()
+{
+link = document.getElementById("link");
+image = document.getElementById("selfie_image").src;
+link.href = image;
+link.click();
+}
